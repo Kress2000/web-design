a631af3a02ac71938bb31a4d3ac401925bc58032
nsanzimfura eric <erickykress1@gmail.com>
2023-01-30 22:01:54 +0200

    web design

--- src/components/BodyContent/BodyContent.js
+++ src/components/BodyContent/BodyContent.js
@@ -3,15 +3,15 @@ import "./BodyContent.scss";
 
 export default function BodyContent() {
   return (
-    <div className='container-fluid bodyContainer p-0 row bg-none'>
-        <div className='col-12 col-md-6 col-lg-4'>
+    <div className='container-fluid bodyContainer p-0 pt-xxl-5 row bg-none'>
+        <div className='col-12 col-md-6 col-lg-4 fisrtBox'>
             <p className='text-xxm lorem'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus molestie massa,
                 id vulputate mauris iaculis nec. Quisque elementum risus vel dui efficitur volutpat.
                 In luctus quam nec sem euismod aliquam. Nullam commodo enim lorem, id ultrices elit mattis at. Nunc tellus augue.
             </p>
             <div className='imgBox'>
-                <img src="/imgs/food-semired.png" className="img img-fluid" width="200" height="200" alt='Img'/>
+                <img src="/imgs/food-semired.png" className="img img-fluid" alt='Img'/>
             </div>
             <div className='loremTitleBox'>
                 <p className='text-sm smallDetails m-0'>Lorem ipsum dolor sit amet, consectetur</p>
@@ -20,11 +20,9 @@ export default function BodyContent() {
         </div>
         <div className='col-12 col-md-6 col-lg-4 middleBox d-flex justify-content-center p-0 align-items-center'>
         </div>
-        <div className='col-12 col-md-6 col-lg-4 rightBox p-0'>
-            <div className='d-flex align-items-lg-center w-100 rightBox-inner h-100 m-0 innerBox '>
-                <img src="/imgs/number.png" className="img img-fluid img-under "  alt='Img'/>
-                <img src="/imgs/foodwhite.png" className="img img-fluid img-upper " alt='Img'/>
-            </div>
+        <div className='col-12 col-md-6 col-lg-4 d-flex rightBox p-0' id='rightBox'>
+                <img src="/imgs/number.png" className="img position-relative img-fluid img-under "  alt='Img'/>
+                <img src="/imgs/foodwhite.png" className="img position-relative img-fluid img-upper " alt='Img'/>
         </div>
     </div>
   )
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,7 +11,8 @@ import "./Footer.scss";
 
 export default function Footer() {
   const [activeFooterLink, setActiveFooterLink]=useState({})
-  const [ActiveLink, setActiveLink]=useState(null)
+  const [ActiveLink, setActiveLink]=useState(null);
+  const [homeActive, setHomeActive] = useState(true);
   const footerArray = [1,2,3,4,5];
   const handleFooterNavLine=(item, index)=>{
     const widthLine = item*20;
@@ -20,6 +21,7 @@ export default function Footer() {
     }
     setActiveFooterLink(styles)
     setActiveLink(index)
+    setHomeActive(false)
   }
   const medias=[
     {
@@ -42,25 +44,25 @@ export default function Footer() {
   ]
   return (
     <div className='footer container-fluid row mt-2 p-0'>
-      <div className='col-12 col-md-6'>
-        <div className='nav footer-nav m-0 p-0'>
+      <div className='col-12 col-md-6 footer-nav-box d-flex flex-column justify-content-center' id='footer-nav-box'>
+        <div className='nav footer-nav m-0 p-0' id='footer-nav'>
           {footerArray.map((item, index)=>(
-            <a className={ActiveLink===index?'footer-link nav-link active p-0 me-3':'footer-link nav-link p-0 me-3'} key={index} onClick={()=>handleFooterNavLine(item, index)}>0{item}</a>
+            <a id={homeActive && index===0?"homeActive": ""} className={ActiveLink===index?'footer-link nav-link active p-0 me-3':'footer-link nav-link p-0 me-3'} key={index} onClick={()=>handleFooterNavLine(item, index)}>0{item}</a>
           ))}
         </div>
         <p className='line w-20 m-0 p-0' style={activeFooterLink?activeFooterLink:{width:"20%"}}></p>
       </div>
-      <div className='col-12 col-md-6 p-1 d-flex justify-content-between rightNav'>
-        <div className='arrows child1'>
-          <span className='text-dark justify-content-end me-4 border-light'>
+      <div className='col-12 col-md-6 p-1 d-flex justify-content-between align-items-center row rightNav' id='rightNav'>
+        <div className='arrows child1 mt-2 mb-2 mb-lg-0 mt-md-0' id='child1'>
+          <span className='text-dark justify-content-end arrow me-4 border-light'>
             <BsArrowLeft  className='smallArrowsL'/>
           </span>
-          <span className='text-dark justify-content-start ms-4 me-0 border-light'>
+          <span className='text-dark justify-content-start arrow ms-4 me-0 border-light'>
             <BsArrowRight className='smallArrowsR' />
           </span>
         </div>
-        <div className='medias child2'>
-          {medias.map((media, i)=> <a key={i} className='text-light me-1 me-lg-1 me-xl-3' href={media.href}>{media.icon}</a>)}
+        <div className='medias child2 d-flex justify-content-between mt-2 mt-md-0' id="medias">
+          {medias.map((media, i)=> <a key={i} className='text-light me-1 me-lg-1 me-xl-3 alink' href={media.href}>{media.icon}</a>)}
         </div>
       </div>
     </div>
